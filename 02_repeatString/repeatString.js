e761157788166ba7bb2a98f4f5fde89e0c8ce86b
Iman <afandyiman5@gmail.com>
2024-11-19 08:51:45 +0700

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    sentence = '';
 
+    if (num < 0){
+        return 'ERROR';
+    }
+    else{
+        for (let i = 1; i<=num; i++){
+            sentence += word;
+        }
+        return sentence;
+    }
+    
 };
 
 // Do not edit below this line
